12d0075f50f05b001c41569298ece78141d6f4c1
successive36 <successive36@Thinks-MacBook-Pro-2.local>
2020-03-18 02:13:49 +0530

    add committe and registration pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -78,7 +78,7 @@
 
 		//smoothscroll
 		$('.menu-item').on('click', function (e) {
-			e.preventDefault();
+			// e.preventDefault();
 			var athis = this;
 			var target = this.hash,
 				menu = target;
